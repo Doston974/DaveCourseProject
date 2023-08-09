da9381306cc3c927c25dd7c23c4ffc8f9e8393a0
Doston Begmatov <@dostonbegmatov1994>
2023-08-09 17:14:26 +0500

    new project dave

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+const Footer = ({ length }) => {
+  const today = new Date();
+
+  return (
+    <footer>
+      <p>
+        {length} List {length === 1 ? "item" : "items"}
+      </p>
+
+      <p style={{ paddingTop: "20px" }}>
+        Copyright &copy; {today.getFullYear()}
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
